822f603d1e95cfa40e7fd84b048e2b30801a1ad4
John Phua <phuajohn0@gmail.com>
2018-10-05 12:39:32 +0800

    Fix typos with "its" (#32)

--- src/main/java/seedu/address/logic/commands/contacts/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/contacts/SelectCommand.java
@@ -16,7 +16,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Selects a person identified using it's displayed index from the address book.
+ * Selects a person identified using its displayed index from the address book.
  */
 public class SelectCommand extends Command {
 
